d0afdcddf81d4637e9eb92fb69f495807acaa457
AlbertPotter <smilepotter0128@gmail.com>
2022-04-09 10:36:01 +0200

    chore: updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ const typography = require("@tailwindcss/typography");
 
 module.exports = {
   darkMode: "class",
-  content: [
-    "./src/**/*.css"
-  ],
+  purge: ["./src/**/*.css"],
   plugins: [forms, typography],
 };
